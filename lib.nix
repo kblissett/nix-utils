64b250ab493cf0256b632f0068e3804775ddfe06
Kevin Blissett <kevin.blissett@parexel.com>
2024-03-01 10:50:22 -0700

    Update name of forAllDefaultSystems

--- lib.nix
+++ lib.nix
@@ -2,5 +2,5 @@ rec {
   nameValuePair = name: value: { inherit name value; };
   genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
   defaultSystems = [ "x86_64-linux"  "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-  forAllDefaultSystems = genAttrs defaultSystems;
+  forEachDefaultSystem = genAttrs defaultSystems;
 }
