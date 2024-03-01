6ed7ecdf457941d8eb95c805cb044f30199c627e
Kevin Blissett <kevin.blissett@parexel.com>
2024-03-01 10:39:15 -0700

    Add default systems

--- lib.nix
+++ lib.nix
@@ -1,4 +1,6 @@
 rec {
   nameValuePair = name: value: { inherit name value; };
   genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+  defaultSystems = [ "x86_64-linux"  "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+  forAllDefaultSystems = genAttrs defaultSystems;
 }
